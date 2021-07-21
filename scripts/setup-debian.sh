38df9ecded7a947840c66f41340817e5b3c43ef8
Yang Sheng Han <progamesigner@gmail.com>
2021-07-21 11:16:48 +0800

    Split devtools from debian script

--- scripts/setup-debian.sh
+++ scripts/setup-debian.sh
@@ -51,52 +51,12 @@ PACKAGE_LIST="\
     sudo \
     tar \
     tzdata \
-"
-
-DEVELOPMENT_PACKAGE_LIST="\
-    coreutils \
-    cpio \
-    cpustat \
-    diffutils \
-    dnsutils \
-    ethtool \
-    findutils \
-    htop \
-    ifstat \
-    iftop \
-    iperf \
-    iproute2 \
-    iptables \
-    iptstate \
-    iputils-arping \
-    iputils-clockdiff \
-    iputils-ping \
-    iputils-tracepath \
-    lsof \
-    lynx \
-    mtr \
-    ncdu \
-    net-tools \
-    netcat-openbsd \
-    nghttp2 \
-    openssl \
-    psmisc \
-    rsync \
-    socat \
-    strace \
-    sysstat \
-    tcpdump \
-    telnet \
-    tree \
-    unzip \
     vim-tiny \
-    wget \
-    xxdiff \
 "
 
-echo "Packages to verify are installed: ${PACKAGE_LIST} ${DEVELOPMENT_PACKAGE_LIST}"
+echo "Packages to verify are installed: ${PACKAGE_LIST}"
 apt-get update
-apt-get install --no-install-recommends -y ${PACKAGE_LIST} ${DEVELOPMENT_PACKAGE_LIST}
+apt-get install --no-install-recommends -y ${PACKAGE_LIST}
 apt-get upgrade --no-install-recommends -y
 
 # Ensure at least the en_US.UTF-8 UTF-8 locale is available
---
+++ scripts/setup-devtools.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+
+# Check the script is run as root
+if [ "$(id -u)" -ne 0 ]; then
+    echo "The script must be run as root. Use sudo, su, or add \"USER root\" to your Dockerfile before running this script."
+    exit 1
+fi
+
+PACKAGE_LIST="\
+    coreutils \
+    cpio \
+    cpustat \
+    diffutils \
+    dnsutils \
+    ethtool \
+    findutils \
+    htop \
+    ifstat \
+    iftop \
+    iperf \
+    iproute2 \
+    iptables \
+    iptstate \
+    iputils-arping \
+    iputils-clockdiff \
+    iputils-ping \
+    iputils-tracepath \
+    lsof \
+    lynx \
+    mtr \
+    ncdu \
+    net-tools \
+    netcat-openbsd \
+    nghttp2 \
+    openssl \
+    psmisc \
+    rsync \
+    socat \
+    strace \
+    sysstat \
+    tcpdump \
+    telnet \
+    tree \
+    unzip \
+    wget \
+    xxdiff \
+"
+
+echo "Install Tools ..."
+
+apt-get update
+apt-get install --no-install-recommends -y ${PACKAGE_LIST}
+apt-get upgrade --no-install-recommends -y
+
+echo "Done!"
