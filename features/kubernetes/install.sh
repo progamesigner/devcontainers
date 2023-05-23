3b852446cddecd30f08184cc93ca59db718ca54f
Han Yang <han.yang@oursky.com>
2023-05-23 09:57:13 +0000

    Suport Helm

--- features/kubernetes/install.sh
+++ features/kubernetes/install.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 KUBECTL_VERSION=${KUBECTL:-${1:-none}}
+HELM_VERSION=${HELM:-${2:-none}}
 
 KUBECTL_SHA256=${KUBECTL_SHA256:-automatic}
+HELM_SHA256=${HELM_SHA256:-automatic}
 
 set -e
 
@@ -42,6 +44,57 @@ if [[ ${KUBECTL_VERSION} != none ]]; then
     if [[ ${KUBECTL_SHA256} != skip ]]; then
         echo "${KUBECTL_SHA256}" | grep "$(sha256sum /usr/local/bin/kubectl | cut -d ' ' -f 1)"
     fi
+
+    echo "Done!"
+fi
+
+if [[ ${HELM_VERSION} != none ]]; then
+    echo "Setup Helm v${HELM_VERSION} ..."
+
+    ARCHITECTURE=""
+    case "$(dpkg --print-architecture)" in
+        amd64) ARCHITECTURE=amd64;;
+        arm64) ARCHITECTURE=arm64;;
+        armel|armhf) ARCHITECTURE=arm;;
+        i386) ARCHITECTURE=386;;
+        *) echo "unsupported architecture"; exit 1 ;;
+    esac
+
+    if [[ ${HELM_VERSION} = latest ]]; then
+        HELM_VERSION=$(curl -sSL https://api.github.com/repos/helm/helm/releases/latest | jq -r ".tag_name")
+    fi
+
+    if [[ ${HELM_VERSION} != v* ]]; then
+        HELM_VERSION=v${HELM_VERSION}
+    fi
+
+    curl -sSL -o /tmp/helm.tar.gz https://get.helm.sh/helm-${HELM_VERSION}-linux-${ARCHITECTURE}.tar.gz
+    curl -sSL -o /tmp/helm.tar.gz.asc https://github.com/helm/helm/releases/download/${HELM_VERSION}/helm-${HELM_VERSION}-linux-${ARCHITECTURE}.tar.gz.asc
+    curl -sSL -o /tmp/helm.tar.gz.sha256 https://get.helm.sh/helm-${HELM_VERSION}-linux-${ARCHITECTURE}.tar.gz.sha256
+    curl -sSL -o /tmp/helm.tar.gz.sha256.asc https://github.com/helm/helm/releases/download/${HELM_VERSION}/helm-${HELM_VERSION}-linux-${ARCHITECTURE}.tar.gz.sha256.asc
+
+    export GNUPGHOME=$(mktemp -d)
+    curl -sSL https://raw.githubusercontent.com/helm/helm/main/KEYS | gpg --import
+    gpg --batch --verify /tmp/helm.tar.gz.asc /tmp/helm.tar.gz
+    gpg --batch --verify /tmp/helm.tar.gz.sha256.asc /tmp/helm.tar.gz.sha256
+    gpgconf --kill all
+    rm -rf ${GNUPGHOME}
+
+    if [[ ${HELM_SHA256} = automatic ]]; then
+        HELM_SHA256=$(cat /tmp/helm.tar.gz.sha256)
+    fi
+
+    echo $HELM_SHA256
+    if [[ ${HELM_SHA256} != skip ]]; then
+        echo "${HELM_SHA256}" | grep "$(sha256sum /tmp/helm.tar.gz | cut -d ' ' -f 1)"
+    fi
+
+    mkdir -p /tmp/helm
+    tar -xz -f /tmp/helm.tar.gz -C /tmp/helm
+    cp -v /tmp/helm/linux-${ARCHITECTURE}/helm /usr/local/bin/helm
+    chmod +x /usr/local/bin/helm
+
+    rm -rf /tmp/helm.tar.gz /tmp/helm.tar.gz.asc /tmp/helm
 
     echo "Done!"
 fi
