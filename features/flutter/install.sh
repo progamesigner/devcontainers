95a79cc7baee547df569cb29354b3d7905f81ae7
Han Yang <han.yang@oursky.com>
2023-05-30 08:36:57 +0000

    Fix Flutter feature

--- features/flutter/install.sh
+++ features/flutter/install.sh
@@ -2,6 +2,7 @@
 
 FLUTTER_VERSION=${VERSION:-${1:-none}}
 FLUTTER_CHANNEL=${CHANNEL:-${2:-stable}}
+USERNAME=${USERNAME:-${1:-vscode}}
 
 set -e
 
@@ -13,13 +14,30 @@ if [[ $(id -u) != 0 ]]; then
     exit 1
 fi
 
+BUILD_PACKAGES=" \
+    clang++ \
+    cmake \
+    ninja-build \
+    pkg-config \
+"
+
 if [[ ${FLUTTER_VERSION} != none ]]; then
     curl -sSL -o /tmp/flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_CHANNEL}/linux/flutter_linux_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.tar.xz
 
+    apt-get update
+    apt-get install --no-install-recommends --yes ${BUILD_PACKAGES}
+    apt-get upgrade --no-install-recommends --yes
+
     mkdir -p /opt/flutter
+    chown -v ${USERNAME}:${USERNAME} /opt/flutter
+
     tar -xJ -f /tmp/flutter.tar.xz -C /opt/flutter --strip-components=1
 
     rm -rf /tmp/flutter.tar.xz
+
+    apt-get autoremove --yes
+    apt-get clean --yes
+    rm -rf /var/lib/apt/lists/*
 
     echo "Done!"
 fi
