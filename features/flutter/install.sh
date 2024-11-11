2e82f4934563c0922405d5301b30215046911ed9
Yang Sheng Han <progamesigner@gmail.com>
2024-11-11 09:10:30 +0000

    Fix flutter feature

--- features/flutter/install.sh
+++ features/flutter/install.sh
@@ -2,7 +2,7 @@
 
 FLUTTER_VERSION=${VERSION:-${1:-none}}
 FLUTTER_CHANNEL=${CHANNEL:-${2:-stable}}
-USERNAME=${USERNAME:-${1:-vscode}}
+USERNAME=${USERNAME:-${3:-vscode}}
 
 set -e
 
@@ -17,22 +17,27 @@ fi
 BUILD_PACKAGES=" \
     clang \
     cmake \
+    lib32z1 \
+    libbz2-1.0 \
+    libglu1-mesa \
     libgtk-3-dev \
-    liblzma-dev \
     ninja-build \
     pkg-config \
+    xz-utils \
+    zip \
 "
 
 if [[ ${FLUTTER_VERSION} != none ]]; then
-    curl -sSL -o /tmp/flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_CHANNEL}/linux/flutter_linux_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.tar.xz
+    echo "Setup Flutter v${FLUTTER_VERSION} ..."
 
     apt-get update
     apt-get install --no-install-recommends --yes ${BUILD_PACKAGES}
     apt-get upgrade --no-install-recommends --yes
 
+    curl -sSL -o /tmp/flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/releases/${FLUTTER_CHANNEL}/linux/flutter_linux_${FLUTTER_VERSION}-${FLUTTER_CHANNEL}.tar.xz
+
     mkdir -p /opt/flutter
     chown -v ${USERNAME}:${USERNAME} /opt/flutter
-
     tar -xJ -f /tmp/flutter.tar.xz -C /opt/flutter --strip-components=1
 
     rm -rf /tmp/flutter.tar.xz
