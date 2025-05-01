1b9d7a7f61209734a87707cd92e2bdd26e76d71a
Yang Sheng Han <progamesigner@gmail.com>
2025-05-01 02:17:58 +0000

    Split devtools feature into multiple features and reuse entrypoint

--- features/devtools/install.sh
+++ features/devtools/install.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-ACT_VERSION=${ACT:-none}
-BUF_VERSION=${BUF:-none}
 CLOUDFLARED_VERSION=${CLOUDFLARED:-none}
-STEP_VERSION=${STEP:-none}
 TAILSCALE_VERSION=${TAILSCALE:-none}
 WATCHMAN_VERSION=${WATCHMAN:-none}
-
-BUF_SHA256=${BUF_SHA256:-automatic}
 
 set -e
 
@@ -73,76 +68,6 @@ case "$(dpkg --print-architecture)" in
     *) echo "unsupported architecture"; exit 1 ;;
 esac
 
-if [[ ${ACT_VERSION} != none ]]; then
-    echo "Setup act v${ACT_VERSION} ..."
-
-    ARCHITECTURE=""
-    case "$(dpkg --print-architecture)" in
-        amd64) ARCHITECTURE=x86_64;;
-        arm64) ARCHITECTURE=arm64;;
-        armhf) ARCHITECTURE=armv7;;
-        i386) ARCHITECTURE=x386;;
-        *) echo "unsupported architecture"; exit 1 ;;
-    esac
-
-    if [[ ${ACT_VERSION} = latest ]]; then
-        ACT_VERSION=$(curl -sSL https://api.github.com/repos/nektos/act/releases/latest | jq -r ".tag_name")
-    fi
-
-    if [[ ${ACT_VERSION} != v* ]]; then
-        ACT_VERSION=v${ACT_VERSION}
-    fi
-
-    curl -sSL -o /tmp/act.tar.gz https://github.com/nektos/act/releases/download/${ACT_VERSION}/act_Linux_${ARCHITECTURE}.tar.gz
-    curl -sSL -o /tmp/act.tar.gz.asc https://github.com/nektos/act/releases/download/${ACT_VERSION}/checksums.txt
-
-    cat /tmp/act.tar.gz.asc | grep "$(sha256sum /tmp/act.tar.gz | cut -d ' ' -f 1)"
-
-    mkdir -p /tmp/act
-    tar -xz -f /tmp/act.tar.gz -C /tmp/act
-    cp -v /tmp/act/act /usr/local/bin/act
-
-    rm -rf /tmp/act /tmp/act.tar.gz.asc /tmp/act.tar.xz
-fi
-
-if [[ ${BUF_VERSION} != none ]]; then
-    echo "Setup buf v${BUF_VERSION} ..."
-
-    ARCHITECTURE=""
-    case "$(dpkg --print-architecture)" in
-        amd64) ARCHITECTURE=x86_64;;
-        arm64) ARCHITECTURE=aarch64;;
-        *) echo "unsupported architecture"; exit 1 ;;
-    esac
-
-    if [[ ${BUF_VERSION} = latest ]]; then
-        BUF_VERSION=$(curl -sSL https://api.github.com/repos/bufbuild/buf/releases/latest | jq -r ".tag_name")
-    fi
-
-    if [[ ${BUF_VERSION} != v* ]]; then
-        BUF_VERSION=v${BUF_VERSION}
-    fi
-
-    curl -sSL -o /tmp/buf https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/buf-Linux-${ARCHITECTURE}
-    curl -sSL -o /tmp/protoc-gen-buf-breaking https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/protoc-gen-buf-breaking-Linux-${ARCHITECTURE}
-    curl -sSL -o /tmp/protoc-gen-buf-lint https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/protoc-gen-buf-lint-Linux-${ARCHITECTURE}
-    curl -sSL -o /tmp/SHASUMS256.txt.asc https://github.com/bufbuild/buf/releases/download/${BUF_VERSION}/sha256.txt
-
-    cat /tmp/SHASUMS256.txt.asc | grep "$(sha256sum /tmp/buf | cut -d ' ' -f 1)"
-    cat /tmp/SHASUMS256.txt.asc | grep "$(sha256sum /tmp/protoc-gen-buf-breaking | cut -d ' ' -f 1)"
-    cat /tmp/SHASUMS256.txt.asc | grep "$(sha256sum /tmp/protoc-gen-buf-lint | cut -d ' ' -f 1)"
-
-    cp -v /tmp/buf /usr/local/bin/buf
-    cp -v /tmp/protoc-gen-buf-breaking /usr/local/bin/protoc-gen-buf-breaking
-    cp -v /tmp/protoc-gen-buf-lint /usr/local/bin/protoc-gen-buf-lint
-
-    chmod +x /usr/local/bin/buf
-    chmod +x /usr/local/bin/protoc-gen-buf-breaking
-    chmod +x /usr/local/bin/protoc-gen-buf-lint
-
-    rm -rf /tmp/buf /tmp/protoc-gen-buf-breaking /tmp/protoc-gen-buf-lint /tmp/SHASUMS256.txt
-fi
-
 if [[ ${CLOUDFLARED_VERSION} != none ]]; then
     echo "Setup cloudflared v${CLOUDFLARED_VERSION} ..."
 
@@ -153,21 +78,6 @@ if [[ ${CLOUDFLARED_VERSION} != none ]]; then
     chmod +x /usr/local/bin/cloudflared
 
     rm -rf /tmp/cloudflared
-fi
-
-if [[ ${STEP_VERSION} != none ]]; then
-    echo "Setup step v${STEP_VERSION} ..."
-
-    curl -sSL -o /tmp/step-cli.tar.gz https://github.com/smallstep/cli/releases/download/v${STEP_VERSION}/step_linux_${STEP_VERSION}_${ARCHITECTURE}.tar.gz
-    curl -sSL -o /tmp/step-cli.tar.gz.asc https://github.com/smallstep/cli/releases/download/v${STEP_VERSION}/checksums.txt
-
-    cat /tmp/step-cli.tar.gz.asc | grep "$(sha256sum /tmp/step-cli.tar.gz | cut -d ' ' -f 1)"
-
-    mkdir -p /tmp/step-cli
-    tar -xz -f /tmp/step-cli.tar.gz -C /tmp/step-cli --strip-components=1
-    cp -v /tmp/step-cli/bin/step /usr/local/bin/step
-
-    rm -rf /tmp/step-cli /tmp/step-cli.tar.gz.asc /tmp/step-cli.tar.gz
 fi
 
 if [[ ${TAILSCALE_VERSION} != none ]]; then
@@ -193,24 +103,6 @@ if [[ ${TAILSCALE_VERSION} != none ]]; then
     mkdir -p /var/run/tailscale
 
     rm -rf /tmp/tailscale /tmp/tailscale.tar.gz
-fi
-
-if [[ ${WATCHMAN_VERSION} != none ]]; then
-    echo "Setup watchman v${WATCHMAN_VERSION} ..."
-
-    if [[ ${WATCHMAN_VERSION} = latest ]]; then
-        WATCHMAN_VERSION=$(curl -sSL https://api.github.com/repos/facebook/watchman/releases/latest | jq -r ".tag_name")
-    fi
-
-    if [[ ${WATCHMAN_VERSION} != v* ]]; then
-        WATCHMAN_VERSION=v${WATCHMAN_VERSION}
-    fi
-
-    curl -sSL -o /tmp/watchman.deb https://github.com/facebook/watchman/releases/download/${WATCHMAN_VERSION}/watchman_ubuntu22.04_${WATCHMAN_VERSION}.deb
-
-    apt-get install --fix-broken --yes /tmp/watchman.deb
-
-    rm -rf /tmp/watchman.deb
 fi
 
 echo "$(cat << 'EOF'
